aab07d8110904bec57ba25d04af5b8ddb55414cf
dkowalczyk <d.kowalczyk@dituel.pl>
2025-02-13 15:28:10 +0100

    feature: add analytics

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import LoadingOverlay from "@/components/shared/LoadingOverlay.vue";
 import Navbar from "@/components//navbar/Navbar.vue";
 import { provide, ref, watch } from "vue";
 import { useLoadingStore } from "./store/loadingStore";
+import { Analytics } from "@vercel/analytics/vue";
 
 const route = useRoute();
 const articleTitle = ref<string | undefined>(undefined);
@@ -29,5 +30,6 @@ watch(
     <main class="flex-grow">
       <RouterView :key="$route.fullPath" />
     </main>
+    <Analytics />
   </div>
 </template>
